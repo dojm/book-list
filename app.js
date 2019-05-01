fc37f14165092d8d1663bd220fc5af42e2d1cbd3
Mary Do <marydo318@gmail.com>
2019-04-30 20:24:51 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+// Book Constructor (creating book object)
+function Book(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI Constructor 
+function UI(){}
+// 3.5) create prototype method for UI - add book to list
+UI.prototype.addBookToList = function(book){
+    const list = document.getElementById('book-list');
+
+    // create tr element
+    const row = document.createElement('tr');
+
+    // Insert col
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `
+    list.appendChild(row);
+}
+// create prototype method - clear fields
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+// create prototype method - show alerts
+UI.prototype.showAlert = function(message, className) {
+    // create div
+    const div = document.createElement('div');
+    // add class 
+    div.className = `alert ${className}`;
+    // add text
+    div.appendChild(document.createTextNode(message));
+    // insert into dom, get parent
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    // insert alert
+    container.insertBefore(div, form);
+    // timeout after 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function(e) {
+
+    // 1) Get form values
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
+
+    // 2) Instantiate book constructor/object
+    const book = new Book(title, author, isbn);
+
+    // 3) Need to add book to the UI - Instantiate UI object
+    const ui = new UI();
+
+    // VALIDATE
+    if(title === '' || author === '' || isbn === '') {
+        // Error alert
+        ui.showAlert('Please fill in all fields', 'error');
+
+    }else{
+
+    // 4) Add book to list
+    ui.addBookToList(book);
+
+    // Show success
+    ui.showAlert('Book Added!', 'success');
+
+    // 5) Clear fields
+    ui.clearFields();
+
+    }
+
+    e.preventDefault();
+});
